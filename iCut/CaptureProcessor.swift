e040d4490dc31340820386e81c4021266234fd38
Nail Sharipov <nailxsharipov@gmail.com>
2022-03-24 14:45:44 +0500

    simple test

---
+++ iCut/CaptureProcessor.swift
@@ -0,0 +1,164 @@
+//
+//  CaptureProcessor.swift
+//  iCut
+//
+//  Created by Nail Sharipov on 24.03.2022.
+//
+
+import AVFoundation
+import UIKit
+import Foundation
+
+final class CaptureProcessor {
+
+    let session: AVCaptureSession
+    private let device: AVCaptureDevice
+    private let output: AVCapturePhotoOutput
+    private let depthOutput: AVCaptureDepthDataOutput
+    private let photoSolver = CaptureProcessorPhotoSolver()
+    private let depthSolver = CaptureProcessorDepthSolver()
+    private let dataOutputQueue = DispatchQueue(label: "CaptureProcessorWorkQueue")
+    
+    
+    var onDidCapturePhoto: ((AVCapturePhoto) -> ())? {
+        get {
+            photoSolver.onDidCapturePhoto
+        }
+        
+        set {
+            photoSolver.onDidCapturePhoto = newValue
+        }
+    }
+    
+    var onDidUpdateDepthePhoto: ((UIImage) -> ())? {
+        get {
+            depthSolver.onDidUpdateDepthePhoto
+        }
+        
+        set {
+            depthSolver.onDidUpdateDepthePhoto = newValue
+        }
+    }
+    
+    init?() {
+        guard let aDevice = AVCaptureDevice.default(.builtInDualCamera, for: .video, position: .back) else { return nil }
+        
+        device = aDevice
+        session = AVCaptureSession()
+
+        output = AVCapturePhotoOutput()
+        depthOutput = AVCaptureDepthDataOutput()
+        depthOutput.setDelegate(depthSolver, callbackQueue: dataOutputQueue)
+        depthOutput.isFilteringEnabled = true
+        
+        guard
+            let input = try? AVCaptureDeviceInput(device: device),
+            session.canAddInput(input),
+            session.canAddOutput(output)
+        else { return nil }
+
+        session.beginConfiguration()
+        
+        session.sessionPreset = .photo
+        session.addInput(input)
+        session.addOutput(output)
+
+        session.addOutput(depthOutput)
+        
+        if let connection = depthOutput.connection(with: .depthData) {
+            connection.isEnabled = true
+            connection.videoOrientation = .portrait
+        } else {
+            print("No AVCaptureConnection")
+        }
+        
+        depthOutput.isFilteringEnabled = false
+
+        session.commitConfiguration()
+        
+        output.isDepthDataDeliveryEnabled = true
+        
+    }
+    
+    deinit {
+        self.stop()
+    }
+    
+    func start() {
+        session.startRunning()
+    }
+    
+    func stop() {
+        session.stopRunning()
+    }
+    
+    func takeShot() {
+        let settings = AVCapturePhotoSettings(format: [AVVideoCodecKey: AVVideoCodecType.hevc])
+        let isDepthData = output.isDepthDataDeliverySupported
+        settings.isDepthDataDeliveryEnabled = isDepthData
+
+        output.capturePhoto(with: settings, delegate: photoSolver)
+    }
+
+}
+
+final class CaptureProcessorPhotoSolver: NSObject, AVCapturePhotoCaptureDelegate {
+    
+    fileprivate var onDidCapturePhoto: ((AVCapturePhoto) -> ())?
+    
+    
+    func photoOutput(_ output: AVCapturePhotoOutput, willBeginCaptureFor resolvedSettings: AVCaptureResolvedPhotoSettings) {
+        print("willBeginCaptureFor")
+    }
+    
+    func photoOutput(_ output: AVCapturePhotoOutput, willCapturePhotoFor resolvedSettings: AVCaptureResolvedPhotoSettings) {
+        print("willCapturePhotoFor")
+    }
+    
+    func photoOutput(_ output: AVCapturePhotoOutput, didCapturePhotoFor resolvedSettings: AVCaptureResolvedPhotoSettings) {
+        print("didCapturePhotoFor")
+        
+    }
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        print("didFinishProcessingPhoto")
+        if let err = error {
+            print(err.localizedDescription)
+            return
+        }
+        onDidCapturePhoto?(photo)
+    }
+
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishCaptureFor resolvedSettings: AVCaptureResolvedPhotoSettings, error: Error?) {
+        print("didFinishCaptureFor")
+        if let err = error {
+            print(err.localizedDescription)
+            return
+        }
+    }
+    
+}
+
+final class CaptureProcessorDepthSolver: NSObject, AVCaptureDepthDataOutputDelegate {
+    
+    fileprivate var onDidUpdateDepthePhoto: ((UIImage) -> ())?
+    
+    func depthDataOutput(_ output: AVCaptureDepthDataOutput, didOutput depthData: AVDepthData, timestamp: CMTime, connection: AVCaptureConnection) {
+        let convertedDepth: AVDepthData
+        let depthDataType = kCVPixelFormatType_DisparityFloat32
+        if depthData.depthDataType != depthDataType {
+            convertedDepth = depthData.converting(toDepthDataType: depthDataType)
+        } else {
+            convertedDepth = depthData
+        }
+
+        let pixelBuffer = convertedDepth.depthDataMap
+//        pixelBuffer.clamp()
+        let ciImage = CIImage(cvPixelBuffer: pixelBuffer)
+        let image = UIImage(ciImage: ciImage)
+        
+        DispatchQueue.main.async { [weak self] in
+            self?.onDidUpdateDepthePhoto?(image)
+        }
+    }
+    
+}
---
+++ iCut/MainViewController.swift
@@ -0,0 +1,117 @@
+//
+//  MainViewController.swift
+//  iCut
+//
+//  Created by Nail Sharipov on 24.03.2022.
+//
+
+import UIKit
+import AVFoundation
+
+final class MainViewController: UIViewController {
+
+    private let photoView = UIView()
+    private let resultView = UIImageView()
+    private let shotButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Shot", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .medium)
+        button.backgroundColor = UIColor.darkGray
+        return button
+    }()
+    private var cameraLayer: CALayer?
+
+    private var captureProcessor: CaptureProcessor?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .orange
+        self.view.addSubview(photoView)
+        self.view.addSubview(resultView)
+        self.view.addSubview(shotButton)
+        
+        AVCaptureDevice.requestAccess(for: .video) { success in
+            if success {
+                DispatchQueue.main.async { [weak self] in
+                    self?.setupCaptureProcessor()
+                }
+            }
+        }
+        
+        shotButton.addTarget(self, action: #selector(takePhoto), for: .touchUpInside)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        captureProcessor?.stop()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+
+        photoView.frame = CGRect(
+            x: 0,
+            y: 0,
+            width: view.bounds.width,
+            height: 0.5 * view.bounds.height
+        )
+        
+        resultView.frame = CGRect(
+            x: 0,
+            y: photoView.frame.maxY,
+            width: view.bounds.width,
+            height: view.bounds.height - photoView.frame.height
+        )
+
+        shotButton.frame = CGRect(
+            x: 0.5 * (view.bounds.width - 100),
+            y: view.bounds.height - 100,
+            width: 100,
+            height: 50
+        )
+        
+        shotButton.layer.cornerRadius = 0.5 * 50
+
+        guard let layer = cameraLayer else { return }
+        layer.frame = photoView.bounds
+    }
+    
+    @objc private func takePhoto() {
+        captureProcessor?.takeShot()
+    }
+    
+    private func setupCaptureProcessor() {
+        self.captureProcessor = CaptureProcessor()
+        guard let processor = self.captureProcessor else { return }
+        
+        processor.onDidCapturePhoto = { [weak self] photo in
+            guard let depthData = photo.depthData else {
+                return
+            }
+            
+            let pixelMap = depthData.converting(toDepthDataType: kCVPixelFormatType_DepthFloat32)
+            let depthDataMap = pixelMap.depthDataMap //AVDepthData -> CVPixelBuffer
+
+            let ciImage = CIImage(cvPixelBuffer: depthDataMap)
+            let image = UIImage(ciImage: ciImage)
+            
+            self?.resultView.image = image
+            self?.resultView.contentMode = .scaleAspectFit
+        }
+        
+        processor.onDidUpdateDepthePhoto = { [weak self] image in
+            self?.resultView.image = image
+            self?.resultView.contentMode = .scaleAspectFit
+        }
+        
+        let layer = AVCaptureVideoPreviewLayer(session: processor.session)
+        layer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        photoView.layer.addSublayer(layer)
+        layer.frame = photoView.bounds
+        cameraLayer = layer
+        
+        processor.start()
+    }
+}
+
